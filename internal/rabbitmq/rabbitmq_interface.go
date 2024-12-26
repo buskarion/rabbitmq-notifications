2b98a49f73bb5d1e09689e438d1ce1e3dd667724
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2024-12-26 14:37:07 -0300

    feat: add rabbitmq interface

---
+++ internal/rabbitmq/rabbitmq_interface.go
@@ -0,0 +1,9 @@
+package rabbitmq
+
+import "github.com/streadway/amqp"
+
+type RabbitMQInterface interface {
+	Consume(queueName, consumerTag string, autoAck, exclusive, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error)
+	Publish(exchange, routingKey string, mandatory, immediate bool, msg amqp.Publishing) error
+	Close() error
+}
